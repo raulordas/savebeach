ccfe0c21c538e53d6a25766f2442a0ad9d9de0df
Andres-93 <andres.pgomez93@gmail.com>
2019-02-02 12:45:31 +0100

    Adapter de alertas realizado Muestra de alertas finalizado controlando acceso al boton dependiento del tipo de usuario.

--- app/src/main/java/uem/dam/sharethebeach/sharethebeach/activities/Alertas_Activity.java
+++ app/src/main/java/uem/dam/sharethebeach/sharethebeach/activities/Alertas_Activity.java
@@ -2,8 +2,14 @@ package uem.dam.sharethebeach.sharethebeach.activities;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +21,7 @@ import java.util.ArrayList;
 
 import uem.dam.sharethebeach.sharethebeach.ContextoCustom;
 import uem.dam.sharethebeach.sharethebeach.R;
+import uem.dam.sharethebeach.sharethebeach.adapters.AdapterAlertas;
 import uem.dam.sharethebeach.sharethebeach.bean.Alerta;
 
 public class Alertas_Activity extends Base_Activity {
@@ -22,63 +29,89 @@ public class Alertas_Activity extends Base_Activity {
 
     private DatabaseReference dbR;
     private ChildEventListener cel;
+
+    AdapterAlertas adaptador;
+    RecyclerView recicler;
+    LinearLayoutManager miLayoutManager;
+    ArrayList<Alerta> lista = new ArrayList<>();
+    FloatingActionButton boton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_alertas_);
 
+        //String id_usuario_creador = ((ContextoCustom) (getApplicationContext())).getUser().getUid();
+        boton = findViewById(R.id.btnAniadirAlerta);
+
+        //Si el usuario que ha entrado no esta registrado no podra añadir alertas
+        if(((ContextoCustom) (getApplicationContext())).getUser() == null){
+
+            boton.setEnabled(false);
+            boton.setVisibility(View.INVISIBLE);
+
+
+        }
+
+
         dbR = FirebaseDatabase.getInstance().getReference().child("Alerta");
 
-        ArrayList<String> lista = new ArrayList<>();
-        lista.add("USU1");
-        lista.add("USU2");
-        lista.add("USU3");
-        lista.add("USU4");
+        recicler = findViewById(R.id.recyclerViewAlertas);
+        recicler.setHasFixedSize(true);
 
-        String key = dbR.push().getKey();
+        miLayoutManager = new LinearLayoutManager(this);
+        adaptador = new AdapterAlertas(lista, this);
 
-        //String id_usuario_creador = ((ContextoCustom) (getApplicationContext())).getUser().getUid();
-
-
-        Alerta alert = new Alerta(key,"id usuario","descripcion","titulo",
-                "id_playas",lista);
+        recicler.setAdapter(adaptador);
+        recicler.setLayoutManager(miLayoutManager);
+        recicler.setItemAnimator(new DefaultItemAnimator());
 
 
-        dbR.child(key).setValue(alert);
+        adaptador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //AL PULSAR UNA ALERTA
+            }
+        });
 
 
-      cel = new ChildEventListener() {
-          @Override
-          public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+        addChildEvent();
+    }
 
+    private void addChildEvent() {
+        if(cel == null) {
+            cel = new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
-              Alerta alertita = dataSnapshot.getValue(Alerta.class);
+                    Alerta alert = dataSnapshot.getValue(Alerta.class);
+                    lista.add(alert);
+                    adaptador.notifyItemInserted(lista.size() - 1);
 
-              System.out.println(alertita);
+                }
 
-          }
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
-          @Override
-          public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                }
 
-          }
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
 
-          @Override
-          public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                }
 
-          }
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
-          @Override
-          public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                }
 
-          }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-          @Override
-          public void onCancelled(@NonNull DatabaseError databaseError) {
-
-          }
-      };
-        dbR.addChildEventListener(cel);
+                }
+            };
+            dbR.addChildEventListener(cel);
+        }
     }
 
     @Override
@@ -89,5 +122,27 @@ public class Alertas_Activity extends Base_Activity {
     @Override
     public boolean setDrawer() {
         return false;
+    }
+
+    public void creacio_Alerta_Pruebas(){
+
+
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("USU1");
+        lista.add("USU2");
+        lista.add("USU3");
+        lista.add("USU4");
+
+        String key = dbR.push().getKey();
+
+
+
+        Alerta alert = new Alerta(key,"id usuario","descripcion","titulo",
+                "id_playas","25/05/2015","09:00AM",lista);
+
+
+        dbR.child(key).setValue(alert);
+
+
     }
 }
