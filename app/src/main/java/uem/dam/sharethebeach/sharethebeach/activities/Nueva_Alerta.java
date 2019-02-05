83fbe81c8affa421116cd903cfb016b544e378a9
Andres-93 <andres.pgomez93@gmail.com>
2019-02-05 17:46:41 +0100

    Añadidas opciones fecha y hora para alertas

--- app/src/main/java/uem/dam/sharethebeach/sharethebeach/activities/Nueva_Alerta.java
+++ app/src/main/java/uem/dam/sharethebeach/sharethebeach/activities/Nueva_Alerta.java
@@ -1,8 +1,16 @@
 package uem.dam.sharethebeach.sharethebeach.activities;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 import uem.dam.sharethebeach.sharethebeach.ContextoCustom;
 import uem.dam.sharethebeach.sharethebeach.R;
@@ -12,6 +20,9 @@ public class Nueva_Alerta extends AppCompatActivity {
 
     Spinner pSpi;
     AdapterSpinner adaptador;
+    private int ano,mes,dia,hora,minutos;
+    TextView fecha;
+    TextView horaTxt;
 
 
     @Override
@@ -23,5 +34,44 @@ public class Nueva_Alerta extends AppCompatActivity {
         pSpi = findViewById(R.id.spPlayas);
         adaptador = new AdapterSpinner(this,((ContextoCustom) (getApplicationContext())).getListadoPlayas());
         pSpi.setAdapter(adaptador);
+
+        fecha = findViewById(R.id.txtMuestraFecha);
+        horaTxt = findViewById(R.id.txtMuestraHora);
     }
+
+
+
+    public void Fecha(View v){
+
+        final Calendar c = Calendar.getInstance();
+         dia = c.get(Calendar.DAY_OF_MONTH);
+         mes = c.get(Calendar.MONTH);
+         ano = c.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                fecha.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+            }
+        }, dia,mes,ano);
+        datePickerDialog.show();
+    }
+
+    public void Hora(View v){
+        final Calendar c = Calendar.getInstance();
+        hora = c.get(Calendar.HOUR_OF_DAY);
+        minutos = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,R.style.DialogTheme, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                horaTxt.setText(hourOfDay + ":" + minute);
+            }
+        },hora,minutos,true);
+        timePickerDialog.show();
+
+
+    }
+
+
 }
