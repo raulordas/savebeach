f99010b1471d85894a828399cbc62a16415de42e
mikiyu <miguel.atletista@gmail.com>
2019-02-15 19:20:51 +0100

    Acerca de Activity

---
+++ app/src/main/java/uem/dam/sharethebeach/sharethebeach/activities/Acerca_De_Activity.java
@@ -0,0 +1,17 @@
+package uem.dam.sharethebeach.sharethebeach.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uem.dam.sharethebeach.sharethebeach.R;
+
+public class Acerca_De_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca__de_);
+
+
+    }
+}
